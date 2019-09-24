d04dd95ac77d23910a17c6056992ebb43903e2d8
Socrates <socrates@Socratess-MacBook-Pro.local>
2019-09-24 17:54:45 +0300

    init push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      posts: []
+    };
+
+  }
+  componentWillMount() {
+    this.getDataAxios()
+  }
+
+  getDataAxios() {
+    axios.get(`https://my-json-server.typicode.com/ayman-uxbert/test-fake-api/posts`)
+      .then(res => {
+        const posts = res.data;
+        this.setState({ posts });
+      })
+  }
+
+  render() {
+    return (  
+      <ul>
+        {this.state.posts.map(post => <li>{post.title}</li>)}
+      </ul>
+    )
+  }
 }
 
 export default App;
